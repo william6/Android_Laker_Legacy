4464505323aaca85373527343777cc6ec63ca7d4
Matthew <william6@github.com>
2014-03-23 15:54:07 -0400

    added files

--- Application/src/edu/gvsu/ll/DonateActivity.java
+++ Application/src/edu/gvsu/ll/DonateActivity.java
@@ -15,11 +15,7 @@ import android.widget.TabHost.TabSpec;
 
 public class DonateActivity extends Activity
 {
-	/*
-	 * I'm planning on keeping this code for good. Once I can get tabs to call activities, this will be
-	 * all set and will work. Lets not edit this commented piece of code. - Matthew
-	 */
-/*
+
 	private WebView myWebView;
 	
 	@Override
@@ -27,7 +23,6 @@ public class DonateActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 
-		//**THIS WORKS** Added for webview 3/6/14
 		myWebView = new WebView(this);
 		myWebView.getSettings().setJavaScriptEnabled(true);
 		final Activity activity = this;
@@ -40,40 +35,6 @@ public class DonateActivity extends Activity
 		
         myWebView.loadUrl("https://secure.gvsu.edu/giving/lakerlegacies");
         setContentView(myWebView);
-		
-*/
-		public void onCreate(Bundle savedInstanceState) 
-		{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.webview);
-		TabHost tabHost=(TabHost)findViewById(R.id.tabHost);
-		tabHost.setup();
-	    ImageView image = null;
-        
-        //Home Page
-        TabSpec spec1=tabHost.newTabSpec("Home");
-        spec1.setIndicator("Home",getResources().getDrawable(R.drawable.home));
-        spec1.setContent(R.id.tab1);
 
-        
-        //Directory
-        TabSpec spec2=tabHost.newTabSpec("Directory");
-        spec2.setIndicator("Directory",getResources().getDrawable(R.drawable.directory));
-        spec2.setContent(R.id.tab2);
-
-        //Near Me
-        TabSpec spec3=tabHost.newTabSpec("Near Me");
-        spec3.setIndicator("Near Me",getResources().getDrawable(R.drawable.maps));
-        spec3.setContent(R.id.tab3);
-        
-        //Donate
-        TabSpec spec4=tabHost.newTabSpec("Donate");
-        spec4.setIndicator("Donate",getResources().getDrawable(R.drawable.donate));
-        spec4.setContent(R.id.tab4);
-        
-        tabHost.addTab(spec1);
-        tabHost.addTab(spec2);
-        tabHost.addTab(spec3);
-        tabHost.addTab(spec4);
 		}
 }
--- Application/src/edu/gvsu/ll/MapView.java
+++ Application/src/edu/gvsu/ll/MapView.java
@@ -1,13 +1,29 @@
 package edu.gvsu.ll;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MapView extends Activity {
-	
+public class MapView extends FragmentActivity {
+    GoogleMap map;
+ 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		setContentView(R.layout.map);
+        setContentView(R.layout.map);
+ 
+        map = ((SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+        if (map == null) {
+            Toast.makeText(this, "Google Maps not available", 
+                Toast.LENGTH_LONG).show();
+        }
     }
+ 
 }
