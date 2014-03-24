29b650a70c38af874c5804965ff5b76e9f6bfd70
Matthew <william6@github.com>
2014-03-24 19:16:21 -0400

    Added waypoints for the pew campus and allendale campus manually.

--- Application/src/edu/gvsu/ll/DonateActivity.java
+++ Application/src/edu/gvsu/ll/DonateActivity.java
@@ -2,16 +2,10 @@ package edu.gvsu.ll;
 
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.webkit.WebViewFragment;
 import android.widget.Toast;
-import android.widget.ImageView;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 
 public class DonateActivity extends Activity
 {
--- Application/src/edu/gvsu/ll/MapView.java
+++ Application/src/edu/gvsu/ll/MapView.java
@@ -12,31 +12,32 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
-public class MapView extends FragmentActivity   {
-   private GoogleMap map;
-   
+public class MapView extends FragmentActivity   { 
    static final LatLng GVSUPEW = new LatLng(42.963826, -85.677772);
    static final LatLng GVSUALL = new LatLng(42.966746, -85.886718);
+   private GoogleMap map;
 		  
    /*The following variables will be used when pulling from the database*/
-   double lat;
-   double lng;
-   LatLng Lat_long = new LatLng(lat,lng);
+   //double lat;
+   //double lng;
+   //LatLng Lat_long = new LatLng(lat,lng);
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
-        
-            
+        //Get's the map
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+            //if map exists
             if (map!=null){
             	
-            //This is just a sample waypoint	
+            //This is just a sample waypoint. Will need to pull from the database to get the correct
+            //coordinates and populate a class that has latitude and longitude.
               Marker pew = map.addMarker(new MarkerOptions().position(GVSUPEW)
                   .title("GVSU - Pew").snippet("Downtown Campus"));
               Marker allendale = map.addMarker(new MarkerOptions()
-                  .position(GVSUALL)
-                  .title("GVSU - Allendale")
+                  .position(GVSUALL).title("GVSU - Allendale")
                   .snippet("Allendale Campus"));
               
               map.moveCamera(CameraUpdateFactory.newLatLngZoom(GVSUPEW, 15));
